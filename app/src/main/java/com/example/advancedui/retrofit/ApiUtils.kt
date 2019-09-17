9f38af766484d63dcfa49d2ec44003ba8b3cb926
vn0zi4e <carlos.rojas6@walmart.com>
2019-09-17 14:47:10 -0500

    Added initial setup to show a toast

--- app/src/main/java/com/example/advancedui/retrofit/ApiUtils.kt
+++
@@ -1,6 +0,0 @@
-package com.example.advancedui.retrofit
-
-object ApiUtils {
-    val soService: ImagesService
-        get() = RetrofitClient.client.create(ImagesService::class.java)
-}
--- app/src/main/java/com/example/advancedui/rv/PostViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.advancedui.rv
-
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel
-import com.example.advancedui.retrofit.ImagesRepository
-
-class PostViewModel : ViewModel() {
-    val posts: LiveData<List<Post>> = ImagesRepository.getPosts()
-}
